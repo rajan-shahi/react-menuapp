b537bd81048e5bf8888d987602b36a17e43a178f
rajan <rajanshahi1267@gmail.com>
2023-08-27 23:00:48 -0700

    add hero components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
   <div>
     <Navbar/>
+    <Hero/>
   </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ function Navbar() {
       {nav ?  <div className=" bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>  :''}
      
       {/* Side drew menu */}
-      <div className={nav ?  ' fixed left-0 top-0  w-[300px] h-screen bg-white z-10 duration-300  ' : ' fixed left-[100%] top-0  w-[300px] h-screen bg-white z-10 duration-300  '}>
+      <div className={nav ?  ' fixed left-0 top-0  w-[300px] h-screen bg-white z-10 duration-300  ' : ' fixed left-[-100%] top-0  w-[300px] h-screen bg-white z-10 duration-300  '}>
         <GrClose  onClick={()=> setNav(!nav)} size={30} className=" absolute right-4 top-4 cursor-pointer" />
         <h2 className=" text-2xl p-4 ">
           Best <span className=" font-bold">Eats</span>
