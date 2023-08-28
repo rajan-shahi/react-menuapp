25104cfe8efc92bdf5ac3a944931185bd4b77726
rajan <rajanshahi1267@gmail.com>
2023-08-28 09:20:37 -0700

    add

--- src/components/HeadLineCards.jsx
+++ src/components/HeadLineCards.jsx
@@ -9,7 +9,7 @@ const HeadLineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
           <p className='px-2'>Through 8/26</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4 rounded-2xl px-1'>Order Now</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
@@ -23,7 +23,7 @@ const HeadLineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
           <p className='px-2'>Added Daily</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4 rounded-2xl px-1'>Order Now</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
@@ -37,7 +37,7 @@ const HeadLineCards = () => {
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
           <p className='px-2'>Tasty Treats</p>
-          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4 rounded-2xl px-1'>Order Now</button>
         </div>
         <img
         className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
