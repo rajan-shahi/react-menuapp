9e8bbd058860fbb26758e1dca49f2b9f1da9fbe0
rajan <rajanshahi1267@gmail.com>
2023-10-11 09:16:02 -0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,9 +27,6 @@ function Navbar() {
           <p className=" bg-black text-white rounded-full p-2 ">Delivery</p>
           <p>Pickup</p>
         </div>
-    loki
-    raajan
-    gita
       </div>
       {/* search */}
       <div className=" bg-gray-200  rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
