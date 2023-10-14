e06d347e77da5983e38583681d68dd694fe5ac51
rajan <rajanshahi1267@gmail.com>
2023-10-13 19:37:00 -0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,8 +37,6 @@ function Navbar() {
           placeholder="search foods"
         />
       </div>
-    loki chaufakain
-    rakan bahadut shahi
       {/* card side */}
       <button className=" bg-black text-white hidden md:flex items-center py-2 rounded-full ">
         <FaShoppingCart size={20} className=" mr-1" />
